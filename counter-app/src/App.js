3d94be59357ecc34f244b446735060ffe7e1f9cc
victordruc <druc92@gmail.com>
2021-07-18 10:55:00 +0300

    update

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 class App extends React.Component {
+
+constructor() {
+  super()
+  this.clickHandle = this.clickHandle.bind(this);
+  // in cazut nostru pentru o simpla apelare a metodei clickHandle() fare utilizarea this ca callback ar fi posipibil fara de apelat constructorul
+}
+
+// varianta 1 
+clickHandle() {
+  console.log("btn clicked")
+}
+
+// varianta 2
+// clickHandle = () => {
+//   console.log("btn clicked")
+// }
+
   render() {
-    return <button> Click </button>
+    return <button onClick={this.clickHandle}> Click </button>
   }
 }
 
